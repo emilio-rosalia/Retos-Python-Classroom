e7c53873e057c132a8b66945f4e0e795222ab95d
emilio-rosalia <emilio@colexiorosaliadecastro.es>
2023-04-20 17:46:19 +0200

    Corrección del ejercicio 3 de Atréve

--- Atrevete/Reto03.py
+++ Atrevete/Reto03.py
@@ -1,5 +1,5 @@
 # Reto 3
-# Halla todos los divisores de un número introducido
+# Halla la descomposición factorial de un número pedido.
 
 # Pedimos un número hasta que de introduzcan solo digitos y no sea 0
 numero = input('Introduce un número entero positivo: ')
@@ -8,12 +8,15 @@ while not (numero.isdigit() and numero !='0') :
 
 numero=int(numero)
 
-i = 2
-while i**i <= numero:
-    if anumero % i:
-        print (i)
-    i = i + 1
-print(i)
+divisores = [] # Lista vacía donde vamos a guardar la descomposición factorial
+i = 2 # El primer divisor primo de un número es el 2
 
-print("Los divisores de", num, "son:")
-divisores(num)
+while i <= numero: 
+    if (numero % i == 0): # Comprobamos si i es divisor del número 
+        divisores.append(i) # Lo guardamos
+        numero = numero/i   # Dividimos el número por el divisor
+    else: # Si no es divisor pruebo con el siguiente
+        i = i+1       
+    
+
+print(f"Los divisores de {numero} son: {divisores}")

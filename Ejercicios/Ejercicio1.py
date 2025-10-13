93ae9a9f80259c0083794e360618cc0097a3f9f7
emilio-rosalia <coordinadortic@colexiorosaliadecastro.es>
2025-10-13 22:24:07 +0200

    Correciones Python 4

--- Ejercicios/Ejercicio1.py
+++
@@ -1,7 +0,0 @@
-# Ejercicio 1
-# Haz un programa que calcule los n primeros múltiplos de un número dado.
-multiplo = int(input("Introduce el número del que quieres saber los múltiplos: "))
-n = int(input("Introduce cuántos múltiplos quieres saber: "))
-print(f"Los {n} primeros múltiplos de {multiplo} son: ")
-for i in range(n):
-    print(multiplo * i, end=" ") # Multiplicamos el número por el rango de 1 a n+1
